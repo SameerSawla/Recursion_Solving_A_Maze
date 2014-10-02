8ca8b8336265894a9ffdc270e3aa5a90a9f91463
Sameer <sameer@Sameer.(none)>
2014-10-02 00:48:52 -0400

    Working solution

--- main.cpp
+++ main.cpp
@@ -18,19 +18,46 @@ bool isValidPoint(char grid[6][6],int x,int y)
 	{
 		return false;
 	}
-	if(grid[x][y]=='#')
+	if(grid[x][y]=='X' || grid[x][y]=='+')
 	{
 		return false;
 	}
 	return true;
 }
 
+bool traceMaze(char grid[6][6],int x,int y)
+{
+	if(!isValidPoint(grid,x,y))
+	{
+		return false;
+	}
+	if(grid[x][y]=='G')
+	{
+		return true;
+	}
+
+	grid[x][y] = '+';
+
+	if(traceMaze(grid,x-1,y)){return true;}
+	if(traceMaze(grid,x,y+1)){return true;}
+	if(traceMaze(grid,x+1,y)){return true;}
+	if(traceMaze(grid,x,y-1)){return true;}
+
+	grid[x][y] = '.';
+
+	return false;
+
+}
+
+
 int main()
 {
-	char grid[6][6] = {{'S','#','#','#','#','#'},{'.','.','.','.','.','#'},{'#','.','#','#','#','#'},{'#','.','#','#','#','#'},{'.','.','.','#','.','G'},{'#','#','.','.','.','#'}};
+	char grid[6][6] = {{'S','X','X','X','X','X'},{'.','.','.','.','.','X'},{'X','.','X','X','X','X'},{'X','.','X','X','X','X'},{'.','.','.','X','.','G'},{'X','X','.','.','.','X'}};
 	cout<<"Initial grid is as follows :\n";
 	printGrid(grid);
-
 	cout<<"\nStarting at : (0,0)\nTracing the path to the Goal\n";
+	cout<<traceMaze(grid,0,0)<<"\n";
+	cout<<"\nFinal grid is as follows :\n";
+	printGrid(grid);
 	return 0;
 }
